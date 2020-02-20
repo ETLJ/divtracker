b987b02ad5963b2b7a1e2f9210e6eb3a7c2f3325
elovell <elovell@liberty.edu>
2020-02-19 21:42:26 -0500

    Initial commit

---
+++ divtracker-backend/src/main/java/com/elovell/divtrackerrest/DivtrackerrestApplication.java
@@ -0,0 +1,13 @@
+package com.elovell.divtrackerrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivtrackerrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DivtrackerrestApplication.class, args);
+	}
+
+}
---
+++ divtracker-backend/src/test/java/com/elovell/divtrackerrest/DivtrackerrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elovell.divtrackerrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DivtrackerrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
