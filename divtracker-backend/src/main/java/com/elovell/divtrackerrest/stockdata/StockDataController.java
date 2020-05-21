424e0a376787a1517efd1b0f7b6a6446e30fabd5
elovell <elovell@liberty.edu>
2020-05-20 20:20:10 -0400

    Add required args constructor annotation to controller.

--- divtracker-backend/src/main/java/com/elovell/divtrackerrest/stockdata/StockDataController.java
+++ divtracker-backend/src/main/java/com/elovell/divtrackerrest/stockdata/StockDataController.java
@@ -1,21 +1,18 @@
 package com.elovell.divtrackerrest.stockdata;
 
 import com.elovell.divtrackerrest.timeseriesdailyadjusted.TimeSeriesDailyAdjusted;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class StockDataController {
 
     private final StockDataService stockDataService;
     private final StockDataResourceAssembler stockDataResourceAssembler;
-
-    public StockDataController(StockDataService stockDataService, StockDataResourceAssembler stockDataResourceAssembler) {
-        this.stockDataService = stockDataService;
-        this.stockDataResourceAssembler = stockDataResourceAssembler;
-    }
 
     @GetMapping("/stock/{symbol}")
     @CrossOrigin
